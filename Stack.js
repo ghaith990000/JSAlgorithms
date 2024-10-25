35146158da193ecc01dfdd49df76ecf8d1dca894
Ghaith Alkhudhiri <ghaith990000@gmail.com>
2024-10-25 20:41:01 +0300

    added Decmial to binary function using stack algorithm

--- Stack.js
+++ Stack.js
@@ -45,3 +45,23 @@ console.log(stack.size());
 console.log(stack.isEmpty());
 stack.clear();
 console.log(stack.isEmpty());
+
+
+// Decimal to Binary conversion using Stack
+function divideBy2(decNumber){
+    var remStack = new Stack(), rem, binaryString = '';
+    
+    while (decNumber > 0){
+        rem = Math.floor(decNumber % 2);
+        remStack.push(rem);
+        decNumber = Math.floor(decNumber / 2);
+    }
+
+    while(!remStack.isEmpty()){
+        binaryString += remStack.pop().toString();
+    }
+
+    return binaryString;
+}
+
+console.log(divideBy2(1244));
