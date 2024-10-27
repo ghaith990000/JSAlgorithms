14080886837d3665abb555542a5373f17b75f11f
Ghaith Alkhudhiri <ghaith990000@gmail.com>
2024-10-27 12:10:12 +0300

    fixed bug in insert method for doublyLinkedList class

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -1,5 +1,5 @@
 class DoublyNode {
-    constructor(element){
+    constructor(element) {
         this.element = element;
         this.next = null;
         this.prev = null;
@@ -7,61 +7,62 @@ class DoublyNode {
 }
 
 class DoublyLinkedList {
-    constructor(){
+    constructor() {
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
 
-    append(element){
+    append(element) {
         const node = new DoublyNode(element);
-        if(!this.head){
+        if (!this.head) {
             this.head = node;
             this.tail = node;
-        }else {
+        } else {
+            node.prev = this.tail;
             this.tail.next = node;
-            this.prev = this.tail;
             this.tail = node;
         }
         this.length++;
     }
 
     // Inserts a new element at the specified position in the list
-    insert(position, element){
-        if(position < 0 || position > this.length){
+    insert(position, element) {
+        if (position < 0 || position > this.length) {
             return false;
         }
 
         const node = new DoublyNode(element);
-        if(position === 0){
+
+        if (position === 0) {
             // Insert at the head
-            if(!this.head){
+            if (!this.head) {
                 this.head = node;
                 this.tail = node;
             } else {
-                node.next = this.next;
+                node.next = this.head;
                 this.head.prev = node;
                 this.head = node;
             }
-        }else if (position === this.length){
+        } else if (position === this.length) {
             // Insert at the tail
             node.prev = this.tail;
             this.tail.next = node;
             this.tail = node;
-        }else {
+        } else {
             let current = this.head;
             let index = 0;
 
-            while(index < position){
+            while (index < position) {
                 current = current.next;
                 index++;
             }
+
             node.next = current;
             node.prev = current.prev;
-            if(current.prev){
+            if (current.prev) {
                 current.prev.next = node;
             }
-            
             current.prev = node;
         }
 
@@ -69,19 +70,19 @@ class DoublyLinkedList {
         return true;
     }
 
-    // Removes thes specified element form the list
-    remove(element){
+    // Removes the specified element from the list
+    remove(element) {
         const index = this.indexOf(element);
         return this.removeAt(index);
     }
 
     // Returns the index of the specified element, or -1 if not found
-    indexOf(element){
+    indexOf(element) {
         let current = this.head;
         let index = 0;
 
-        while(current){
-            if(current.element === element){
+        while (current) {
+            if (current.element === element) {
                 return index;
             }
             current = current.next;
@@ -90,28 +91,27 @@ class DoublyLinkedList {
         return -1;
     }
 
-    removeAt(position){
-        if(position < 0 || position >= this.length){
+    removeAt(position) {
+        if (position < 0 || position >= this.length) {
             return null;
         }
 
         let current = this.head;
 
-        if(position === 0){
+        if (position === 0) {
             this.head = current.next;
-            if(this.head){
+            if (this.head) {
                 this.head.prev = null;
-            }else {
+            } else {
                 this.tail = null;
             }
-        }else if(position === this.length - 1){
+        } else if (position === this.length - 1) {
             current = this.tail;
             this.tail = current.prev;
             this.tail.next = null;
-        }else {
+        } else {
             let index = 0;
-
-            while (index < position){
+            while (index < position) {
                 current = current.next;
                 index++;
             }
@@ -124,19 +124,19 @@ class DoublyLinkedList {
         return current.element;
     }
 
-    isEmpty(){
+    isEmpty() {
         return this.length === 0;
     }
 
-    size(){
+    size() {
         return this.length;
     }
 
-    toString(){
+    toString() {
         let current = this.head;
         let string = '';
 
-        while(current){
+        while (current) {
             string += current.element + (current.next ? ' <-> ' : '');
             current = current.next;
         }
@@ -144,15 +144,16 @@ class DoublyLinkedList {
     }
 }
 
+// Testing the implementation
 const doublyLinkedList = new DoublyLinkedList();
 doublyLinkedList.append('Node 1');
 doublyLinkedList.append('Node 2');
 doublyLinkedList.insert(1, 'Node 1.5');
-console.log(doublyLinkedList.toString());
-console.log(doublyLinkedList.remove('Node 1.5'));
-console.log(doublyLinkedList.toString());
-console.log(doublyLinkedList.indexOf('Node 2'));
-console.log(doublyLinkedList.removeAt(0));
-console.log(doublyLinkedList.toString());
-console.log(doublyLinkedList.isEmpty());
-console.log(doublyLinkedList.size());
+console.log(doublyLinkedList.toString());  // Node 1 <-> Node 1.5 <-> Node 2
+console.log(doublyLinkedList.remove('Node 1.5'));  // Node 1.5
+console.log(doublyLinkedList.toString());  // Node 1 <-> Node 2
+console.log(doublyLinkedList.indexOf('Node 2'));  // 1
+console.log(doublyLinkedList.removeAt(0));  // Node 1
+console.log(doublyLinkedList.toString());  // Node 2
+console.log(doublyLinkedList.isEmpty());  // false
+console.log(doublyLinkedList.size());  // 1
