164016e3570ea5ef0455b5efbf84d081824a0168
Ghaith Alkhudhiri <ghaith990000@gmail.com>
2024-10-31 11:52:54 +0300

    implement the union operation

--- Set.js
+++ Set.js
@@ -34,6 +34,17 @@ class Set {
     values(){
         return Object.keys(this.items);
     }
+
+    union(otherSet){
+        const unionSet = new Set();
+        // Add values from the current set
+        this.values().forEach(value => unionSet.add(value));
+
+        // Add values from the other set
+        otherSet.values().forEach(value => unionSet.add(value));
+
+        return unionSet;
+    }
 }
 
 const set = new Set();
@@ -48,3 +59,17 @@ console.log(set.values());
 set.clear();
 console.log(set.values());
 console.log(set.size());
+
+// union example
+const setA = new Set();
+setA.add(1);
+setA.add(2);
+setA.add(3);
+
+const setB = new Set();
+setB.add(3);
+setB.add(4);
+setB.add(5);
+
+const unionSet = setA.union(setB);
+console.log(unionSet.values());
